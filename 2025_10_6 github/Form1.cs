9074b911d2b62335c17c308bf8ff0499fbc82e98
ProzaOK <90964021+ProzaOK@users.noreply.github.com>
2025-10-06 08:20:09 +0200

    Přidejte soubory projektu.

---
+++ 2025_10_6 github/Form1.cs
@@ -0,0 +1,15 @@
+namespace _2025_10_6_github
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
