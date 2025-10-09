1ea959f7469cd8ee2aa8a20ba4a83c704aa2166e
ProzaOK <90964021+ProzaOK@users.noreply.github.com>
2025-10-09 14:10:51 +0200

    už to má počítání

--- 2025_10_6 github/MyCalculator.cs
+++ 2025_10_6 github/MyCalculator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,32 @@ namespace _2025_10_6_github
 {
     internal class MyCalculator
     {
+        public string Input;
         public MyCalculator()
         {
             
+        }
+        public double Vypocet(string input)
+        {
+            double vysledek;
+
+            try
+            {
+                // Vyhodnocení výrazu (používáme DataTable.Compute pro jednoduché příklady)
+                var data = new DataTable();
+                vysledek = Convert.ToDouble(data.Compute(input, ""));
+            }
+            catch
+            {
+                vysledek = double.NaN; // Pokud dojde k chybě, vrátíme NaN
+            }
+            return vysledek;
+
+
+            //lev int
+            //prav int
+            //vys int
+            //vys int position x
         }
     }
 }
